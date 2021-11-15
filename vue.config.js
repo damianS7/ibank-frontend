89875a3d06c9d95d465a6d677724ca204d444c1e
damianS7 <>
2021-11-15 16:04:35 +0100

    base del proyecto

---
+++ vue.config.js
@@ -0,0 +1,47 @@
+const options = {
+    transformAssetUrls: {
+      video: ['src', 'poster'],
+      source: 'src',
+      img: 'src',
+      image: 'xlink:href',
+      'b-avatar': 'src',
+      'b-img': 'src',
+      'b-img-lazy': ['src', 'blank-src'],
+      'b-card': 'img-src',
+      'b-card-img': 'src',
+      'b-card-img-lazy': ['src', 'blank-src'],
+      'b-carousel-slide': 'img-src',
+      'b-embed': 'src'
+    }
+  }
+  
+  module.exports = {
+    // Modifica las rutas usadas por npm build
+    // publicPath: './',
+    pages: {
+      index: {
+        entry: 'src/app.js'
+      }
+    },
+    chainWebpack: config => {
+      config.module
+        .rule('vue')
+        .use('vue-loader')
+        .loader('vue-loader')
+        .tap(options => {
+          options["transformAssetUrls"] = {
+            'img': 'src',
+            'image': 'xlink:href',
+            'b-img': 'src',
+            'b-img-lazy': ['src', 'blank-src'],
+            'b-card': 'img-src',
+            'b-card-img': 'img-src',
+            'b-carousel-slide': 'img-src',
+            'b-embed': 'src'
+          };
+  
+          return options;
+        });
+    }
+  }
+  
