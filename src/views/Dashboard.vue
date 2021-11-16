a1f010687e96f2aae4a7acc303019644520201ea
damianS7 <josepwnz@gmail.com>
2021-11-16 23:02:00 +0100

    agregado boton logout

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,7 +1,9 @@
 <template>
   <b-col cols="12">
     <b-row class="mb-1 widget"> </b-row>
-    <b-row class="mb-1 widget"> </b-row>
+    <b-row class="mb-1 widget">
+      Dashboard <b-button @click="logout()">Logout</b-button>
+    </b-row>
     <b-row class="mb-1 widget"> </b-row>
   </b-col>
 </template>
@@ -9,7 +11,11 @@
 <script>
 import { mapGetters } from "vuex";
 const components = {};
-const methods = {};
+const methods = {
+  logout() {
+    this.$store.dispatch("security/logout");
+  },
+};
 
 const computed = {
   ...mapGetters({}),
